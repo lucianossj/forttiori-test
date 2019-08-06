b378b3fa21c9292bae9e9d184397f0e17003d723
Luciano S. Junior <luciano.ssj@hotmail.com>
2019-08-06 15:28:30 -0300

    Unit tests update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,86 +1,49 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-
+import { TestSuite } from '../../../pip-frontend/client/src/angular5/test-utils/test-suite';
+import { IdForm } from './forms/idForm';
+import { GetItineraryService } from './services/get-itinerary.service';
 import { MinibusLinesService } from './services/minibus-lines.service';
-
-import { HttpClient } from '@angular/common/http';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { BusLinesService } from './services/bus-lines.service';
 import { BusNameForm } from './forms/busNameForm';
-import { MinibusNameForm } from './forms/minibusNameForm';
-import { IdForm } from '../../../pocdimed/src/app/forms/Idform';
+import { AppComponentStub as stub } from './app.component.stub';
 
-describe('AppComponent - Tests', () => {
+describe('AppComponent', () => {
 
   let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   let minibusLinesService: MinibusLinesService;
-  let http: HttpClient;
+  let busLinesService: BusLinesService;
+  let getItineraryService: GetItineraryService;
+  let busNameForm: BusNameForm;
+  let idForm: IdForm;
+
+  TestSuite.configure();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
+      providers: [
+        { provide: MinibusLinesService, useClass: stub ,},
+        { provide: BusLinesService, useClass: stub },
+        { provide: GetItineraryService, useClass: stub },
+        { provide: BusNameForm, useClass: stub },
+        { provide: IdForm, useClass: stub }
       ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    }).compileComponents()
+    .then(() => {
+      minibusLinesService = TestBed.get(MinibusLinesService);
+      busLinesService = TestBed.get(BusLinesService);
+      getItineraryService = TestBed.get(GetItineraryService);
+      busNameForm = TestBed.get(BusNameForm);
+      idForm = TestBed.get(IdForm);
 
-    minibusLinesService = new MinibusLinesService(http);
-
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+    });
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    component = fixture.debugElement.componentInstance;
-    expect(component).toBeTruthy();
+  it('The component has been defined', () => {
+    expect(component).toHaveBeenCalled();
   });
-
-  it('should test if the request has response', () => {
-
-    let array = minibusLinesService.search();
-
-    expect(array).not.toBeNull();
-
-  });
-
 });
 
-/*
-
-private minibusLinesService: MinibusLinesService,
-  private busLinesService: BusLinesService,
-  private getItineraryService: GetItineraryService,
-  private minibusNameForm: MinibusNameForm,
-  private busNameForm: BusNameForm,
-  private idForm: IdForm
-
-beforeEach(async(() => {
-  TestBed.configureTestingModule({
-    imports: [
-      RouterTestingModule
-    ],
-    declarations: [
-      AppComponent
-    ],
-  }).compileComponents();
-}));
-
-it('should create the app', () => {
-  const fixture = TestBed.createComponent(AppComponent);
-  const app = fixture.debugElement.componentInstance;
-  expect(app).toBeTruthy();
-});
-
-it(`should have as title 'forttiori-test'`, () => {
-  const fixture = TestBed.createComponent(AppComponent);
-  const app = fixture.debugElement.componentInstance;
-  expect(app.title).toEqual('forttiori-test');
-});
-
-it('should render title in a h1 tag', () => {
-  const fixture = TestBed.createComponent(AppComponent);
-  fixture.detectChanges();
-  const compiled = fixture.debugElement.nativeElement;
-  expect(compiled.querySelector('h1').textContent).toContain('Welcome to forttiori-test!');
-});*/
